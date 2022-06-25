629d0ce4dbee03c38ddad114dabdddd04afb7f77
okancingoz <88890682+okancingoz@users.noreply.github.com>
2022-06-25 18:55:10 +0300

    Update Program.cs

--- LinkedList/SinglyLinkedList/Program.cs
+++ LinkedList/SinglyLinkedList/Program.cs
@@ -1,4 +1,5 @@
-﻿var linkedList = new SinglyLinkedList<int>();
+/* 
+var linkedList = new SinglyLinkedList<int>();
 linkedList.AddFirst(1);
 linkedList.AddFirst(2);
 linkedList.AddFirst(3);
@@ -37,3 +38,36 @@ foreach (var item in linkedList)
 {
     Console.WriteLine(item);
 }
+
+var arr = new char[] {'a','b','c','d'};
+
+var linkedList2 = new SinglyLinkedList<char>(arr);
+
+foreach (var item in linkedList2)
+{
+    Console.WriteLine(item);
+} 
+
+var rnd = new Random();
+var initial = Enumerable.Range(1,5).OrderBy(x=> rnd.Next()).ToList();
+//var query = from item in linkedList where item%2 == 1 select item;
+*/;
+var list = new List<int>{1,2,3,4,5};
+var linkedList = new SinglyLinkedList<int>(list);
+foreach (var item in linkedList)
+{
+    Console.WriteLine(item);
+}
+linkedList.Remove(1);
+linkedList.Remove(2);
+linkedList.Remove(5);
+linkedList.Remove(3);
+
+foreach (var item in linkedList)
+{   
+    System.Console.Write(item + "-");
+}
+//linkedList.Where(x=> x>5).ToList().ForEach(x=>Console.Write(x + " "));
+
+
+
